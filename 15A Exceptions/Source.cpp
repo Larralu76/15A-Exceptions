ac5e7c11c7250ee190a94b1c19d6e6c2a157349e
larralu <larralu@aol.com>
2021-12-05 16:09:12 -0500

    Char function for prototype complete

--- 15A Exceptions/Source.cpp
+++ 15A Exceptions/Source.cpp
@@ -22,3 +22,25 @@ class caseConversionException
 {
 
 };
+
+char character(char start, int offset)
+{
+	int startAsciiValue = int(start);
+	int target = startAsciiValue + offset;
+
+	try
+	{
+		if (startAsciiValue < 65 || (startAsciiValue > 90 && startAsciiValue < 97) || startAsciiValue > 122)
+		{
+			throw invalidCharacterException();
+		}
+		else if (target < 65 || (target > 90 && target < 97) || target > 122)
+		{
+			throw invalidRangeException();
+		}
+		else if ((target >= 65 && target <= 90 && startAsciiValue >= 95 && startAsciiValue <= 122) || (target >= 95 && target <= 122 && startAsciiValue >= 65 && startAsciiValue <= 90))
+		{
+			throw caseConversionException();
+		}
+	}
+}
