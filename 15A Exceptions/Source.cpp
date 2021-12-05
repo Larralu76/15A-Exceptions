8e8d24a084e1e6d2b5f43bfb14758a5cd0a36e37
larralu <larralu@aol.com>
2021-12-05 15:34:58 -0500

    Classes for handles finished

---
+++ 15A Exceptions/Source.cpp
@@ -0,0 +1,24 @@
+//Laura Luke
+//12-05-2021
+//CIS 1202 501
+//15a Exceptions
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class invalidCharacterException
+{
+
+};
+
+class invalidRangeException
+{
+
+};
+
+class caseConversionException
+{
+
+};
